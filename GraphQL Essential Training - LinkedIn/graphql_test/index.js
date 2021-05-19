aabfbca7162603cca12155a81860170c3af36738
Sedrick Strozier <slstrozier@gmail.com>
2021-05-19 17:38:59 -0400

    What is the resolver and its role?

--- GraphQL Essential Training - LinkedIn/graphql_test/index.js
+++ GraphQL Essential Training - LinkedIn/graphql_test/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import schema  from './schema';
 import { graphqlHTTP } from 'express-graphql';
+import resolvers from './resolvers';
 
 const app = express();
 
@@ -8,34 +9,8 @@ app.get('/', (req, res) => {
     res.send("GraphQL is amazing");
 });
 
-const friendDatabase = {};
+const root = resolvers;
 
-class Friend{
-    constructor(id, {firstName, lastName, gender, email}) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.igenderd = gender;
-        this.email = email;
-
-    }
-}
-
-const root = { friend: () => {
-    return {
-        "id" : 21423424,
-        "firstName" : "Sedrick",
-        "lastName" : "Strozier",
-        "gender" : "Male",
-        "email" : "slstrozier@gmail.com"
-    }
-},  
-    createFriend: ({input}) => {
-        let id = require('crypto').randomBytes(10).toString('hex');
-        friendDatabase[id] = input;
-        return new Friend(id, input);
-    }
-};
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
---
+++ GraphQL Essential Training - LinkedIn/graphql_test/resolvers.js
@@ -0,0 +1,30 @@
+import { resolve } from 'path';
+
+const friendDatabase = {};
+
+class Friend{
+    constructor(id, {firstName, lastName, gender, email}) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.igenderd = gender;
+        this.email = email;
+
+    }
+}
+
+const resolvers = { friend: () => {
+    
+    getFriend: ({ id }) => {
+        return new Friend(id, friendDatabase[id])
+}
+},  
+    createFriend: ({input}) => {
+        let id = require('crypto').randomBytes(10).toString('hex');
+        friendDatabase[id] = input;
+        return new Friend(id, input);
+    }
+};
+
+
+export default resolvers;
