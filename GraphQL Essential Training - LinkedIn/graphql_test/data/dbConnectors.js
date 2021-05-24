ee9ae768411275a6ad4958cbe055ae0c0a06738a
Sedrick Strozier <slstrozier@gmail.com>
2021-05-24 00:47:00 -0400

    finalize the database connectors

---
+++ GraphQL Essential Training - LinkedIn/graphql_test/data/dbConnectors.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+//mongo connection
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/friends', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const friendSchema = new mongoose.Schema({
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    Gender: {
+        type: String
+    },
+    age: {
+        type: Number
+    },
+    language: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    contacts: {
+        type: Array
+    }
+});
+
+const Friends = mongoose.model('friends', friendSchema);
+
+export { Friends }
